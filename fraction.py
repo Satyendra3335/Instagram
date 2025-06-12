0ae39733071e80282f1009d9c2b4c718e6f81399
Satyendra3335 <satyendrasinghfzd184@gmail.com>
2025-06-12 12:39:01 +0530

    Changes made

--- fraction.py
+++ fraction.py
@@ -8,3 +8,19 @@ class Fraction:
         temp_num=self.num * other.den + other.num * self.den
         temp_den=self.den * other.den
         return "{}/{}".format(temp_num,temp_den)
+    
+    def __sub__(self,other):
+        temp_num=self.num*other.den-other.num*self.den
+        temp_den=self.den*other.den
+        return "{}/{}".format(temp_num,temp_den)
+
+    def __mul__(self,other):
+        temp_num=self.num*other.num
+        temp_den=self*other.den
+        return "{}/{}".format(temp_num,temp_den)
+    
+    def __truediv__(self,other):
+        temp_num=self.num*other.den
+        temp_den=self.den*other.num
+        return "{}/{}".format(temp_num,temp_den)
+
