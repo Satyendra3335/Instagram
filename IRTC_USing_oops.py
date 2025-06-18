148a0644fda71e53d61a9da8ae88dc2710c32c08
Satyendra3335 <satyendrasinghfzd184@gmail.com>
2025-06-18 21:08:28 +0530

    apply walllet balance

--- IRTC_USing_oops.py
+++ IRTC_USing_oops.py
@@ -31,42 +31,45 @@ users = {
         "wallet":4022
     }
 }
+
 class IRTC:
     def __init__(self):
-        user = input("enter your username: ").strip().lower()
+        self.user = input("enter your username: ").strip().lower()  
         passw = input("enter your password: ").strip()
-        if user in users and users[user]["password"] == passw:
-            print(f"\n Login Successful! Welcome, {user}\n")
+        if self.user in users and users[self.user]["password"] == passw:
+            print(f"\n Login Successful! Welcome, {self.user}\n")
             self.calculate_fare()
-
         else:
             print("\n Invalid username or password.\n")
+
     def calculate_fare(self):
-        stations={
-                "A":0,
-                "B":5,
-                "C":10,
-                "D":15,
-                "E":20,
-                "F":25
-                }
+        stations = {
+            "A": 0,
+            "B": 5,
+            "C": 10,
+            "D": 15,
+            "E": 20,
+            "F": 25
+        }
         print("Available station")
         for i in stations:
-            print(stations )
-        start=input("enter starting station").upper().strip()
-        end=input("enter the ending station ").upper().strip()
-        if start not in stations and end not in stations:
+            print(stations)  
+        start = input("enter starting station").upper().strip()
+        end = input("enter the ending station ").upper().strip()
+        if start not in stations or end not in stations: 
             print("Invalid Stations")
             return
-        dist=abs(stations[start]-stations[end])
-        rate_per_km=50
-        fare=dist*rate_per_km
-    
+
+        dist = abs(stations[start] - stations[end])
+        rate_per_km = 50
+        fare = dist * rate_per_km
 
         print(f"distance from {start} and {end}")
         print(f"fare amount {fare}")
-        rest_money=abs(fare-['wallet'])
-        print(rest_money)
-              
+
+        wallet_balance = users[self.user]["wallet"]  
+        rest_money = wallet_balance - fare
+        print(f"Remaining balance after fare: {rest_money if rest_money >= 0 else 'Insufficient Balance'}")
+
 
 IRTC()
